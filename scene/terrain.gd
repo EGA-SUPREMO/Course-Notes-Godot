e346d190a220e2862386017ab84d8f7f1dbb171e
EGA-SUPREMO <cxiesestro@gmail.com>
2024-08-04 15:32:46 -0400

    clean code for clip polygons

--- scene/terrain.gd
+++ scene/terrain.gd
@@ -52,31 +52,13 @@ func clip(poly: PackedVector2Array, global_terrain_position: Vector2):
 	for collision_body in island_holder.get_children():
 		var collision_polygon = collision_body.get_child(0)
 		
-		#print("missil " + str(poly))
-		#print(collision_polygon.global_position.x)
-		#print(map_size.x/2)
-		#var offset_poly_island = Transform2D(0,
-			#Vector2(offset_position.x,
-				#offset_position.y)) * collision_polygon.polygon
 		var offset_position = Vector2(-collision_body.global_position.x,
 			-collision_body.global_position.y)
-		#print(-collision_body.rotation)
 		offset_position = offset_position.rotated(-collision_body.rotation)
+		
 		var offset_poly := Transform2D(-collision_body.rotation,
 			offset_position) * poly
-		#var offset_poly = Transform2D(0, #-collision_polygon.rotation,
-			#Vector2(-global_terrain_position.x,
-			#-global_terrain_position.y)) * poly
-		#offset_poly = poly
-		var poligono := Polygon2D.new()
-		poligono.polygon = offset_poly
-		add_child(poligono)
-	
-		
-		#var offset_position_body = Transform2D(
-		#	0, Vector2(position_body.x, position_body.y)) * collision_polygon.polygon
 		var res = Geometry2D.clip_polygons(collision_polygon.polygon, offset_poly)
-		#var res1 = Geometry2D.clip_polygons(offset_poly_island, offset_poly)
 		
 		
 		if res.size() == 0:
@@ -84,13 +66,7 @@ func clip(poly: PackedVector2Array, global_terrain_position: Vector2):
 			
 		for i in range(res.size()):
 			
-			#var new_res_position := Array()
-			#for new_polygon in res:# da el res grande antes de anadirle los nuevos
-			#	var p = Transform2D(0, offset_position) * new_polygon
-			#	new_res_position.push_back(p)
-			#var clipped_collision = new_res_position[i]
 			var clipped_collision = res[i]
-			#clipped_collision = Transform2D(0, Vector2(-map_size.x/2, -map_size.y/2)) * res[i]
 			# These are awkward single or two-point floaters.
 			if clipped_collision.size() < 3:
 				continue
@@ -98,26 +74,8 @@ func clip(poly: PackedVector2Array, global_terrain_position: Vector2):
 			if i == 0:
 				collision_polygon.set_deferred("polygon", res[0])
 			else:
-				
-				print("new_res {")
-				#print(res1 == res)
-				#print(new_res_position == res)
-				print("}")
-				
-				
-				
-				
 				var collider := CollisionPolygon2D.new()
 				var body := RigidBody2D.new()
-				#body.lock_rotation = true
-				#body.rotation = deg_to_rad(90)
-				#var sprite_collider := Sprite2D.new()
-				#var sprite_rigidbody := Sprite2D.new()
-				#sprite_collider.texture = preload("res://assets/sprites/player_hud/shield_0.png")
-				#sprite_rigidbody.texture = preload("res://assets/sprites/player_hud/shield_0.png")
-				#sprite_collider.scale.x = 0.5 
-				#collider.add_child(sprite_collider)
-				#body.add_child(sprite_rigidbody)
 				body.collision_layer = 3
 				body.collision_mask = 3
 				collider.polygon = clipped_collision
@@ -126,7 +84,6 @@ func clip(poly: PackedVector2Array, global_terrain_position: Vector2):
 				#body.global_position.y -= map_size.y/2
 				body.center_of_mass_mode = RigidBody2D.CENTER_OF_MASS_MODE_CUSTOM
 				body.center_of_mass = Vector2(0, 0)
-				#print(body.center_of_mass)
 				island_holder.call_deferred("add_child", body)
 				body.call_deferred("add_child", collider)
 			#var island := Polygon2D.new()
