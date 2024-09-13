91e5cb3d68c9c42163503ab78046ef6ab9313ccb
EGA-SUPREMO <cxiesestro@gmail.com>
2024-09-13 13:24:31 -0400

    add music and sfx

--- scripts/missile.gd
+++ scripts/missile.gd
@@ -8,12 +8,14 @@ signal explosion
 @onready var missile = $"."
 @onready var collision_shape_2d = $CollisionShape2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sfx_explotion: AudioStreamPlayer2D = $SFX_Explotion
 
 
 func _ready():
 	damage = 30
 	missile.mass = (collision_shape_2d.shape.radius * 2) * collision_shape_2d.shape.height
 	timer.start(0.015)
+	sfx_explotion.pitch_scale = randf() + 0.75
 
 func _physics_process(delta):
 	new_rotation = atan2(linear_velocity.y, linear_velocity.x)
