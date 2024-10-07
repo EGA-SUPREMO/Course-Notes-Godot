d04b64984cae3d66505e2c4dd775ed2ffbf6eb8c
EGA-SUPREMO <cxiesestro@gmail.com>
2024-10-07 13:15:35 -0400

    fix velocity of minibombs in hotshower

--- scene/missile_hotshower.gd
+++ scene/missile_hotshower.gd
@@ -7,7 +7,10 @@ var has_fallen:= false
 const MISSILE = preload("res://scene/missile.tscn")
 
 func _ready() -> void:
-	super._ready()
+	damage = 30
+	missile.mass = collision_shape_2d.shape.size.x * collision_shape_2d.shape.size.y
+	timer.start(0.015)
+	sfx_explotion.pitch_scale = randf() + 0.75
 	
 func _process(delta: float) -> void:
 	if linear_velocity.y > 0 and not has_fallen:
@@ -29,18 +32,18 @@ func drop_bomb():
 	if has_fallen:
 		return
 	has_fallen = true
-	for i in range(1, number_missiles+1):
+	
+	for i in range(-number_missiles/2, number_missiles/2 + 1):
 		var child_missile = MISSILE.instantiate()
 		child_missile.collision_height = 2
 		
 		child_missile.rotation = rotation
-		child_missile.position.y = position.y
-		child_missile.position.x = position.x + ((i-number_missiles/2) * 10)
+		child_missile.position.y = position.y# - collision_shape_2d.shape.size.y/2 
+		child_missile.position.x = position.x + number_missiles * 1
 		
-		var step = number_missiles / float(number_missiles - 1) if number_missiles > 1 else 0
 		var step_multiplier = linear_velocity * 0.05
 		
-		child_missile.linear_velocity = linear_velocity + (step * Vector2(i,i) * step_multiplier)
+		child_missile.linear_velocity = linear_velocity + (Vector2(i,i) * step_multiplier)
 		child_missile.who_shoot = who_shoot
 		
 		child_missile.collision_layer = 4  # Layer 4 (Mini child_missiles)
