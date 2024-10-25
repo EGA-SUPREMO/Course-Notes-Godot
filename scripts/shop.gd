dad08a442c959791ae0a0bc856d8b7c30c4316b8
EGA-SUPREMO <cxiesestro@gmail.com>
2024-10-25 18:59:52 -0400

    add incomplete function create selectors

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -7,6 +7,9 @@ extends Control
 
 @onready var consumables: HBoxContainer = $VBoxContainer/Missiles/Consumables
 @onready var selector: Control = $Selector
+@onready var selectors: Control = $Selectors
+
+var list_labels_selector: Array
 
 var navigable_items: Array
 var current_item_selected = 0
@@ -22,6 +25,29 @@ func _ready() -> void:
 	for current in traits.get_children():
 		navigable_items.append(current)
 	navigable_items.append(next_match_button)
+	
+	create_selectors()
+	
+func create_selectors():
+	var selector_player: Array
+	for player in range(MatchManager.number_players):
+		list_labels_selector.append(Array())
+		for container in selector.get_children():
+			for item in container.get_children():
+				list_labels_selector[player].append(item)
+				item.visible = false
+		selector_player.append(selector.duplicate(0))
+		
+	var number_labels_step = floori(12/MatchManager.number_players)
+	print(number_labels_step)
+	for i in range(0, MatchManager.number_players):
+		for j in range(i * number_labels_step, number_labels_step * (i + 1)):
+			print("player " + str(i) + ", selector: " + str(j))
+			#selector_player.get_children()[0].get_children()[j - i * number_labels_step].visible = true
+			list_labels_selector[i][j].visible = true
+			print(list_labels_selector[i][j])
+		selector_player[i].visible = true
+		selectors.add_child(selector_player[i])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -35,7 +61,7 @@ func _process(_delta: float) -> void:
 		if current_item_selected >= navigable_items.size():
 			current_item_selected = 0
 	
-	selector.position = navigable_items[current_item_selected].global_position
+	selectors.get_children()[0].position = navigable_items[current_item_selected].global_position
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		if navigable_items[current_item_selected] == next_match_button:
